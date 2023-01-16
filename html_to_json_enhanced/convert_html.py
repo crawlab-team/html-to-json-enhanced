32ed9c178347d6919ce1234f04bb82a18432da3d
Marvin Zhang <tikazyq@163.com>
2023-01-16 16:13:23 +0800

    fix: duplicated id issue

--- html_to_json_enhanced/convert_html.py
+++ html_to_json_enhanced/convert_html.py
@@ -5,103 +5,137 @@ from typing import List, Iterator, Union
 import bs4
 
 
-def _debug(debug, message, prefix=''):
-    """Print the given message if debugging is true."""
-    if debug:
-        print('{}{}'.format(prefix, message))
-        # add a newline after every message
-        print('')
+class HtmlConverter(object):
+    def __init__(
+        self,
+        html_section,
+        debug: bool,
+        capture_element_values: bool,
+        capture_element_attributes: bool,
+        with_id: bool,
+    ):
+        self.html_section = html_section
+        self.debug = debug
+        self.capture_element_values = capture_element_values
+        self.capture_element_attributes = capture_element_attributes
+        self.with_id = with_id
 
+        self._element_id = 0
 
-def _record_element_value(element, json_output, with_id: bool, element_id: int, parent_id: int = None) -> int:
-    """Record the html element's value in the json_output."""
-    element = element.strip()
-    if element != '\n' and element != '':
-        if json_output.get('_value'):
-            json_output['_values'] = [json_output['_value']]
-            json_output['_values'].append(element)
-            del json_output['_value']
-        elif json_output.get('_values'):
-            json_output['_values'].append(element)
-        else:
-            json_output['_value'] = element
+    @staticmethod
+    def log_debug(debug, message, prefix=''):
+        """Print the given message if debugging is true."""
+        if debug:
+            print('{}{}'.format(prefix, message))
+            # add a newline after every message
+            print('')
 
-        # record the element's id
-        if with_id and json_output.get('_id') is None:
-            json_output['_id'] = element_id
-            element_id += 1
+    def _get_element_id(self):
+        element_id = self._element_id
+        self._element_id += 1
+        return element_id
 
-        # record the element's parent id
-        if with_id and parent_id is not None and json_output['_parent'] is None:
-            json_output['_parent'] = parent_id
-
-    return element_id
-
-
-def _iterate(
-    html_section,
-    json_output: dict,
-    count: int,
-    debug: bool,
-    capture_element_values: bool,
-    capture_element_attributes: bool,
-    with_id: bool,
-    element_id: int = 0,
-    parent_id: int = None,
-):
-    _debug(debug, '========== Start New Iteration ==========', '    ' * count)
-    _debug(debug, 'HTML_SECTION:\n{}'.format(html_section))
-    _debug(debug, 'JSON_OUTPUT:\n{}'.format(json_output))
-
-    for part in html_section:
-        if not isinstance(part, str):
-            # construct the new json output object
-            if not json_output.get(part.name):
-                json_output[part.name] = list()
-
-            # construct the new json child object
-            new_json_output_for_subparts = dict()
-
-            # record the element's attributes
-            if part.attrs and capture_element_attributes:
-                new_json_output_for_subparts = {'_attributes': part.attrs}
+    def _record_element_value(self, element, json_output, with_id: bool, parent_id: int = None) -> int:
+        """Record the html element's value in the json_output."""
+        element = element.strip()
+        if element != '\n' and element != '':
+            if json_output.get('_value'):
+                json_output['_values'] = [json_output['_value']]
+                json_output['_values'].append(element)
+                del json_output['_value']
+            elif json_output.get('_values'):
+                json_output['_values'].append(element)
+            else:
+                json_output['_value'] = element
 
             # record the element's id
-            if with_id:
-                # assign id
-                new_json_output_for_subparts['_id'] = element_id
+            if with_id and json_output.get('_id') is None:
+                json_output['_id'] = self._get_element_id()
 
-                # record parent id
-                if parent_id is not None:
-                    new_json_output_for_subparts['_parent'] = parent_id
+            # record the element's parent id
+            if with_id and parent_id is not None and json_output['_parent'] is None:
+                json_output['_parent'] = parent_id
 
-                # increment the id
-                element_id += 1
+    def iterate(
+        self,
+        html_section,
+        json_output: dict,
+        count: int,
+        debug: bool,
+        capture_element_values: bool,
+        capture_element_attributes: bool,
+        with_id: bool,
+        parent_id: int = None,
+    ):
+        self.log_debug(debug, '========== Start New Iteration ==========', '    ' * count)
+        self.log_debug(debug, 'HTML_SECTION:\n{}'.format(html_section))
+        self.log_debug(debug, 'JSON_OUTPUT:\n{}'.format(json_output))
 
-            # record the element's tag
-            new_json_output_for_subparts['_tag'] = part.name
+        for part in html_section:
+            if not isinstance(part, str):
+                # construct the new json output object
+                if not json_output.get(part.name):
+                    json_output[part.name] = list()
 
-            # increment the count
-            count += 1
+                # construct the new json child object
+                new_json_output_for_subparts = dict()
 
-            # append to json output
-            json_output[part.name].append(
-                _iterate(
-                    part,
-                    new_json_output_for_subparts,
-                    count,
-                    debug=debug,
-                    capture_element_values=capture_element_values,
-                    capture_element_attributes=capture_element_attributes,
-                    with_id=with_id,
-                    element_id=element_id,
-                    parent_id=new_json_output_for_subparts['_id'],
+                # record the element's attributes
+                if part.attrs and capture_element_attributes:
+                    new_json_output_for_subparts = {'_attributes': part.attrs}
+
+                # record the element's id
+                if with_id:
+                    # assign id
+                    new_json_output_for_subparts['_id'] = self._get_element_id()
+
+                    # record parent id
+                    if parent_id is not None:
+                        new_json_output_for_subparts['_parent'] = parent_id
+
+                # record the element's tag
+                new_json_output_for_subparts['_tag'] = part.name
+
+                # increment the count
+                count += 1
+
+                # append to json output
+                json_output[part.name].append(
+                    self.iterate(
+                        part,
+                        new_json_output_for_subparts,
+                        count,
+                        debug=debug,
+                        capture_element_values=capture_element_values,
+                        capture_element_attributes=capture_element_attributes,
+                        with_id=with_id,
+                        parent_id=new_json_output_for_subparts['_id'],
+                    )
                 )
-            )
-        else:
-            if capture_element_values:
-                element_id = _record_element_value(part, json_output, with_id, element_id, parent_id)
-    return json_output
+            else:
+                if capture_element_values:
+                    self._record_element_value(part, json_output, with_id, parent_id)
+        return json_output
+
+    def convert(self):
+        """Convert the html string to json."""
+        soup = bs4.BeautifulSoup(self.html_section, 'html.parser')
+        children = [child for child in soup.contents]
+        json_output = self.iterate(
+            children,
+            {},
+            0,
+            debug=self.debug,
+            capture_element_values=self.capture_element_values,
+            capture_element_attributes=self.capture_element_attributes,
+            with_id=self.with_id,
+        )
+
+        return json_output
+
+
+def _debug(debug, message, prefix=''):
+    HtmlConverter.log_debug(debug, message, prefix)
 
 
 def convert(
@@ -111,20 +145,13 @@ def convert(
     capture_element_attributes: bool = True,
     with_id: bool = True,
 ):
-    """Convert the html string to json."""
-    soup = bs4.BeautifulSoup(html_string, 'html.parser')
-    children = [child for child in soup.contents]
-    json_output = _iterate(
-        children,
-        {},
-        0,
-        debug=debug,
-        capture_element_values=capture_element_values,
-        capture_element_attributes=capture_element_attributes,
-        with_id=with_id,
-    )
-
-    return json_output
+    return HtmlConverter(
+        html_string,
+        debug,
+        capture_element_values,
+        capture_element_attributes,
+        with_id,
+    ).convert()
 
 
 def iterate(json_output: dict, visited: set = None, parent: dict = None) -> Iterator[dict]:
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = [
 
 setup(
     name='html-to-json-enhanced',
-    version='1.0.1',
+    version='1.0.2',
     description='Convert html to json.',
     long_description=readme,
     long_description_content_type='text/markdown',
